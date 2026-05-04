bff0e75d7053ae1bc49eaf9b8798e0735eb63657
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:39:00 +0530

    Added binary_search.c

--- Searching/binary_search.c
+++ Searching/binary_search.c
@@ -1,44 +1,40 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int binarySearch(int array[], int n, int key) {
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (array[mid] == key) {
+int binarySearch(int arr[], int n, int key) {
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == key)
             return mid;
-        }
-        else if (array[mid] < key) {
-            low = mid + 1;
-        }
-        else {
-            high = mid - 1;
-        }
+        else if (arr[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
+
     return -1;
 }
 
 int main() {
     int n;
-    printf("Enter number of elements in ascending order: ");
-    scanf("%d", &n);
+    cin >> n;
 
-    int array[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter element at index %d: ", i);
-        scanf("%d", &array[i]);
-    }
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
     int key;
-    printf("Enter the number to search: ");
-    scanf("%d", &key);
+    cin >> key;
 
-    int result = binarySearch(array, n, key);
+    int result = binarySearch(arr, n, key);
 
-    if (result != -1) {
-        printf("Element found at index %d\n", result);
-    } else {
-        printf("Element not found in the array\n");
-    }
+    if (result != -1)
+        cout << result;
+    else
+        cout << -1;
 
     return 0;
 }
