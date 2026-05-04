1ab1d36514d425bc6a325fd69365bb4ae442e1a0
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:35:19 +0530

    Added selection_sort.cpp

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -2,48 +2,31 @@
 using namespace std;
 
 void selectionSort(int arr[], int n) {
-    int i, j, minIndex;
-
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-
-        for (j = i + 1; j < n; j++) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
-
-        // swap
         int temp = arr[i];
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     int n;
-
-    cout << "Enter number of elements: ";
     cin >> n;
 
     int arr[n];
-
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    }
 
     selectionSort(arr, n);
 
-    cout << "Sorted array:\n";
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
