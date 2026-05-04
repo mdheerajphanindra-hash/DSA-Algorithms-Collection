2d4b24fada0ca8afc0dd14fdfb7f2ddf5e0e8dcf
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:36:40 +0530

    Added merge_sort.c

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -1,61 +1,67 @@
-//Merge Sort Technique
-#include<stdio.h>
-void merge(int arr[], int left, int middle, int right) {
-    int i, j, k;
-    int NL = middle - left + 1;
-    int NR = right - middle;
-    int L[NL], R[NR];
+#include <iostream>
+using namespace std;
 
-    for (i = 0; i < NL; i++)
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
-    for (j = 0; j < NR; j++)
-        R[j] = arr[middle + 1 + j];
 
-    i = 0; j = 0; k = left;
-    while (i < NL && j < NR) {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++];
-        else
-            arr[k++] = R[j++];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    while (i < NL)
-        arr[k++] = L[i++];
-    while (j < NR)
-        arr[k++] = R[j++];
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
-}
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
 }
 
 int main() {
     int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    cin >> n;
 
     int arr[n];
-    printf("Enter %d elements: ", n);
     for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    printf("Original array:\n");
-    printArray(arr, n);
+        cin >> arr[i];
 
     mergeSort(arr, 0, n - 1);
 
-    printf("Sorted array:\n");
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
