339506f56ac865b85094bb153b77e8eb6a1b1138
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:32:58 +0530

    Added selection_sort.cpp

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
+    int i, j, minIndex;
 
     for (i = 0; i < n - 1; i++) {
         minIndex = i;
@@ -13,33 +14,35 @@ void selectionSort(int arr[], int n) {
         }
 
         // swap
-        temp = arr[i];
+        int temp = arr[i];
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
     }
 }
 
 void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
-    int n, i;
+    int n;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    cout << "Enter number of elements: ";
+    cin >> n;
 
     int arr[n];
 
-    printf("Enter elements:\n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
     selectionSort(arr, n);
 
-    printf("Sorted array:\n");
+    cout << "Sorted array:\n";
     printArray(arr, n);
 
     return 0;
