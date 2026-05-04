7718881f3918a01cde361ffdb9abba8174c72bae
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:37:47 +0530

    Added bubble_sort.c

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // swap
-                temp = arr[j];
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -15,28 +13,18 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-}
-
 int main() {
-    int n, i;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    int n;
+    cin >> n;
 
     int arr[n];
-
-    printf("Enter elements:\n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
     bubbleSort(arr, n);
 
-    printf("Sorted array:\n");
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
