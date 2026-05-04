32579212d83a9cfaa723806b04acfdb465096176
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:36:15 +0530

    Added quick_sort.cpp

--- Sorting/quick_sort.cpp
+++ Sorting/quick_sort.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];  // last element as pivot
+    int pivot = arr[high];
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            // swap arr[i] and arr[j]
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
     }
 
-    // place pivot in correct position
     int temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
@@ -26,37 +24,23 @@ int partition(int arr[], int low, int high) {
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-
-        // recursively sort elements before and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     int n;
-
-    cout << "Enter number of elements: ";
     cin >> n;
 
     int arr[n];
-
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    }
 
     quickSort(arr, 0, n - 1);
 
-    cout << "Sorted array:\n";
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
