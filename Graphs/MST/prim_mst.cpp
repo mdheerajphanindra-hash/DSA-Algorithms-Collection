259399cc902664a3a5122342a5aae1fc0fabe20c
mdheerajphanindra-hash <mdheerajphanindra@gmail.com>
2026-05-04 14:42:01 +0530

    Added prim_mst.cpp

--- Graphs/MST/prim_mst.cpp
+++ Graphs/MST/prim_mst.cpp
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <limits.h>
+#include <iostream>
+#include <climits>
+using namespace std;
 
 int main() {
     int V;
-    printf("Enter number of vertices: ");
-    scanf("%d", &V);
+    cin >> V;
 
     int graph[100][100];
-    printf("Enter adjacency matrix:\n");
     for (int i = 0; i < V; i++)
         for (int j = 0; j < V; j++)
-            scanf("%d", &graph[i][j]);
+            cin >> graph[i][j];
 
     int selected[100] = {0};
     selected[0] = 1;
+
     int edgeCount = 0, mstWeight = 0;
 
-    printf("Edges in MST (Prim's):\n");
     while (edgeCount < V - 1) {
         int min = INT_MAX, x = 0, y = 0;
+
         for (int i = 0; i < V; i++) {
             if (selected[i]) {
                 for (int j = 0; j < V; j++) {
@@ -32,12 +32,15 @@ int main() {
                 }
             }
         }
-        printf("%d - %d : %d\n", x, y, graph[x][y]);
+
+        cout << x << " " << y << " " << graph[x][y] << endl;
+
         mstWeight += graph[x][y];
         selected[y] = 1;
         edgeCount++;
     }
 
-    printf("Total weight of MST: %d\n", mstWeight);
+    cout << mstWeight;
+
     return 0;
 }
